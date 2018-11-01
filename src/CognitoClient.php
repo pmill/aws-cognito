ba509f42ef3c8a638e7929f92f6ddefb40dee5db
Bjoern Heneka <bheneka@bmac2.speedport.ip>
2018-11-01 14:59:26 +0100

    Update the getUser in order to retrive an Result Object again

--- src/CognitoClient.php
+++ src/CognitoClient.php
@@ -214,7 +214,7 @@ class CognitoClient
                 'Username' => $username,
                 'UserPoolId' => $this->userPoolId,
             ]);
-            return $response['data'];
+            return $response;
         } catch (Exception $e) {
             throw CognitoResponseException::createFromCognitoException($e);
         }
