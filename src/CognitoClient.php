bc9328421fc8adb26b8fe0d6274d08db6d3f5e55
Bjoern Heneka <bheneka@bmac2.speedport.ip>
2018-11-01 21:15:38 +0100

    add Get group function for a username

--- src/CognitoClient.php
+++ src/CognitoClient.php
@@ -216,7 +216,7 @@ class CognitoClient
                 'Username' => $username,
                 'UserPoolId' => $this->userPoolId,
             ]);
-            return $response;
+            return $response['data'];
         } catch (Exception $e) {
             throw CognitoResponseException::createFromCognitoException($e);
         }
