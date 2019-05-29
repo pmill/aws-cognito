6550d6ea4cac6c0f05e239fbf814934cef76400d
Maik Schmieder <maik.schmieder@hella-gutmann.com>
2019-05-29 12:47:58 +0200

    Use curl for downloading the jwks file because of proxy

--- src/CognitoClient.php
+++ src/CognitoClient.php
@@ -308,7 +308,13 @@ class CognitoClient
             $this->userPoolId
         );
 
-        return file_get_contents($url);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$url);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
     }
 
     /**
